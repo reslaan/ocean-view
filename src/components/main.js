e6d33e1f8e743e97661fcd08de80e2e051b12220
Reslaan <reslaan2012@gmail.com>
2023-10-23 00:40:28 +0300

    set up

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+export const Main = () => {
+  return (
+    <div className="h-[90vh]  flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-2 ">
+        <h1 className="text-6xl font-bold ">شركة أوشن فيو العقارية</h1>
+        <p className="font-semibold text-xl text-gray-700">
+          متخصصون في إدارة العقارات
+        </p>
+      </div>
+    </div>
+  );
+};
